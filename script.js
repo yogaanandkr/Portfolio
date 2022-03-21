607b942ad6f8f4425dcd51061c8769de713cf83a
Yoga Anand K.R <yokeshanand762001@gmail.com>
2022-03-21 23:15:22 +0530

    projects added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 window.addEventListener('scroll', function(){
     const nava = this.document.querySelector('.navbar')
     nava.classList.toggle('sticky', window.scrollY > 10)
+
+
 })
 
+
+// owl carousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
